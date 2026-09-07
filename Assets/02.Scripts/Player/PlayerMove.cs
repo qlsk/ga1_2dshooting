39d9bf3c03de7847336d74625a039aad2768a644
qlsk <jhanggu0625@gmail.com>
2026-09-07 15:36:11 +0900

    feat: 플레이어 이동 애니메이션 적용

--- Assets/02.Scripts/Player/PlayerMove.cs
+++ Assets/02.Scripts/Player/PlayerMove.cs
@@ -2,8 +2,16 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
+    Animator _animator;
+
     private void Start()
     {
+    }
+
+    private void Awake()
+    {
+        // 애니메이터 컴포넌트에 대한 참조를 가져와서 할당한다.
+        _animator = GetComponent<Animator>();
     }
 
     public float Speed = 5f;
@@ -19,6 +27,8 @@ public class PlayerMove : MonoBehaviour
         Vector2 dir = new Vector2(h, v);
         dir = dir.normalized;
         transform.position += Speed * Time.deltaTime * (Vector3)dir;
+
+        _animator.SetInteger("x", (int)dir.x);
 
         if (transform.position.y < MinPositionY)
         {
