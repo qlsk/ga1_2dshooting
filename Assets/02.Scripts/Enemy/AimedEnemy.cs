1971dda0d767351774ce5379b8391891632e8ba3
qlsk <jhanggu0625@gmail.com>
2026-09-08 10:23:38 +0900

    feat: 적이 플레이어를 바라보도록 회전하는 기능 추가

--- Assets/02.Scripts/Enemy/AimedEnemy.cs
+++ Assets/02.Scripts/Enemy/AimedEnemy.cs
@@ -4,17 +4,27 @@ public class AimedEnemy : Enemy
 {
     private GameObject _player;
     private Vector2 _direction;
+    private float _rotation;
+    private float _x;
+    private float _y;
 
     private void Start()
     {
         _player = GameObject.FindWithTag("Player");
 
         _direction = _player.transform.position - transform.position;
+        _x = _direction.x;
+        _y = _direction.y;
         _direction.Normalize();
+
+        _rotation = Mathf.Atan2(_y, _x) * Mathf.Rad2Deg;
+        Debug.Log(_rotation);
+        transform.rotation = Quaternion.Euler(0f, 0f, 90 + _rotation);
     }
 
     private void Update()
     {
-        transform.Translate(_moveSpeed * Time.deltaTime * _direction);
+        // transform.Translate(_moveSpeed * Time.deltaTime * _direction);
+        transform.position += _moveSpeed * Time.deltaTime * (Vector3)_direction;
     }
 }
--- Assets/02.Scripts/Enemy/HomingEnemy.cs
+++ Assets/02.Scripts/Enemy/HomingEnemy.cs
@@ -4,6 +4,9 @@ public class HomingEnemy : Enemy
 {
     private GameObject _player;
     private Vector2 _direction;
+    private float _rotation;
+    private float _x;
+    private float _y;
 
     private void Start()
     {
@@ -13,7 +16,12 @@ public class HomingEnemy : Enemy
     private void Update()
     {
         _direction = _player.transform.position - transform.position;
+        _x = _direction.x;
+        _y = _direction.y;
         _direction.Normalize();
+
+        _rotation = Mathf.Atan2(_y, _x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0f, 0f, 90 + _rotation);
         transform.Translate(_moveSpeed * Time.deltaTime * _direction);
     }
 }
