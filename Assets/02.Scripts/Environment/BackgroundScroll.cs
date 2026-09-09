5db79c4282204d6aad2f30a6dfab404b6f28a96e
qlsk <jhanggu0625@gmail.com>
2026-09-09 14:08:59 +0900

    feat: 패럴랙스 스크롤링 (시차 효과)

--- Assets/02.Scripts/Environment/BackgroundScroll.cs
+++ Assets/02.Scripts/Environment/BackgroundScroll.cs
@@ -13,6 +13,7 @@ public class BackgroundScroll : MonoBehaviour
     private void Update()
     {
         _offsetY += _scrollSpeed * Time.deltaTime;
+        // TODO: MaterialPropertyBlock을 활용한 최적화
         _material.mainTextureOffset = new Vector2(0, _offsetY);
     }
 }
