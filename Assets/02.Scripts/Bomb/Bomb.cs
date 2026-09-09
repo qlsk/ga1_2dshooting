7ed6aa660560c6957dfdd31f8c25b28ec4315b2a
qlsk <jhanggu0625@gmail.com>
2026-09-09 10:54:55 +0900

    feat: Bomb 커졌다가 줄어드는 기능 추가

--- Assets/02.Scripts/Bomb/Bomb.cs
+++ Assets/02.Scripts/Bomb/Bomb.cs
@@ -3,19 +3,40 @@ using UnityEngine;
 public class Bomb : MonoBehaviour
 {
     private float _disappearTime = 3f;
-    private float _disappearTimer = 0f;
+    private float _timer = 0f;
+    private float _scale = 0f;
+    private float _scaleStayTime = 0.5f;
+    private float _scaleReductionTime = 2.5f;
+    private float _scaleReductionTimer = 0f;
+    [SerializeField] private GameObject _deathByBombEffectPrefab;
 
     private void Start()
     {
+        transform.localScale = new Vector3(0f, 0f, 0f);
     }
 
     private void Update()
     {
-        _disappearTimer += Time.deltaTime;
-        if (_disappearTimer >= _disappearTime)
+        _timer += Time.deltaTime;
+        if (_timer >= _disappearTime)
         {
             Destroy(gameObject);
-            _disappearTimer = 0f;
+            _timer = 0f;
+        }
+
+        if (_timer < _scaleStayTime)
+        {
+            _scale += Time.deltaTime * 0.03f;
+            transform.localScale += new Vector3(_scale, _scale, _scale);
+        }
+        else if (_timer > _scaleReductionTime)
+        {
+            _scale += Time.deltaTime * 0.03f;
+            transform.localScale -= new Vector3(_scale, _scale, _scale);
+        }
+        else
+        {
+            _scale = 0;
         }
     }
 
@@ -23,6 +44,7 @@ public class Bomb : MonoBehaviour
     {
         if (other.CompareTag("Enemy"))
         {
+            Instantiate(_deathByBombEffectPrefab, other.transform.position, Quaternion.identity);
             Destroy(other.gameObject);
         }
     }
