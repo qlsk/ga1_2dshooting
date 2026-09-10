bf6c730925d404ff85a8e27ac5c2ba6a4cde1f7a
qlsk <jhanggu0625@gmail.com>
2026-09-10 14:07:00 +0900

    feat: 자동 이동 script에 아이템 관련 로직 추가
    - 아이템이 플레이어에 가장 가깝고, y가 -0.5f 이하일 때 아이템 추적

--- Assets/02.Scripts/Player/PlayerAutoMove.cs
+++ Assets/02.Scripts/Player/PlayerAutoMove.cs
@@ -4,8 +4,19 @@ public class PlayerAutoMove : MonoBehaviour
 {
     [SerializeField] private float _speed;
 
-    private GameObject _target = null;
+    private GameObject _targetEnemy = null;
+    private GameObject _targetItem = null;
     [SerializeField] private float _stopTrackingY = -2;
+    private float _enemyDistance;
+    private float _itemDistance;
+
+    enum target
+    {
+        Enemy,
+        Item
+    }
+
+    private target _target;
 
     private void Start()
     {
@@ -13,20 +24,28 @@ public class PlayerAutoMove : MonoBehaviour
 
     private void Update()
     {
-        if (_target == null || _target.transform.position.y < _stopTrackingY)
+        if (_targetEnemy == null || _targetItem == null || _targetEnemy.transform.position.y < _stopTrackingY)
         {
             FindNearestTarget();
         }
 
-        Move();
+        switch (_target)
+        {
+            case target.Enemy:
+                MoveToEnemy();
+                break;
+            case target.Item:
+                MoveToItem();
+                break;
+        }
     }
 
-    private void Move()
+    private void MoveToEnemy()
     {
-        if (_target == null) return;
+        if (_targetEnemy == null) return;
 
         // 2. 방향을 구한다.
-        Vector3 diff = _target.transform.position - transform.position;
+        Vector3 diff = _targetEnemy.transform.position - transform.position;
         Vector3 direction = diff;
 
         // 적과 나와의 y축 차이가 3보다 크면 앞으로 가고 아니라면 뒤로 가게
@@ -45,16 +64,56 @@ public class PlayerAutoMove : MonoBehaviour
         transform.position += _speed * Time.deltaTime * direction;
     }
 
-    void FindNearestTarget()
+    private void MoveToItem()
+    {
+        if (_targetItem == null) return;
+
+        Vector3 direction = _targetItem.transform.position - transform.position;
+        direction.Normalize();
+        transform.position += _speed * Time.deltaTime * direction;
+    }
+
+    public void FindNearestTarget()
     {
         // 1. 타겟을 구한다.
-        GameObject[] targets = GameObject.FindGameObjectsWithTag("Enemy");
-        if (targets.Length == 0) return;
+        GameObject[] targetsEnemy = GameObject.FindGameObjectsWithTag("Enemy");
+        if (targetsEnemy.Length != 0)
+        {
+            _enemyDistance = FindNearestEnemy(targetsEnemy);
+        }
 
-        _target = targets[0];
-        float minDistance = float.MaxValue;
+        GameObject[] targetsItem = GameObject.FindGameObjectsWithTag("Item");
+        if (targetsItem.Length != 0)
+        {
+            _itemDistance = FindNearestItem(targetsItem);
+        }
+
+        _target = _enemyDistance > _itemDistance ? target.Item : target.Enemy;
+    }
+
+    public float FindNearestItem(GameObject[] items)
+    {
+        _targetItem = items[0];
+        float itemMinDistance = float.MaxValue;
+        foreach (GameObject item in items)
+        {
+            float distance = Vector2.Distance(transform.position, item.transform.position);
+            if (distance < itemMinDistance && item.transform.position.y < -0.5f)
+            {
+                itemMinDistance = distance;
+                _targetItem = item;
+            }
+        }
+
+        return itemMinDistance;
+    }
+
+    public float FindNearestEnemy(GameObject[] enemies)
+    {
+        _targetEnemy = enemies[0];
+        float enemyMinDistance = float.MaxValue;
         // 1-1. 가장 가까운 타겟을 찾는다.
-        foreach (GameObject enemy in targets)
+        foreach (GameObject enemy in enemies)
         {
             if (enemy.transform.position.y < _stopTrackingY)
             {
@@ -63,12 +122,14 @@ public class PlayerAutoMove : MonoBehaviour
 
             // 거리를 구해서
             float distance = Vector2.Distance(transform.position, enemy.transform.position);
-            if (distance < minDistance) // 저장된 거리보다 짧다면
+            if (distance < enemyMinDistance) // 저장된 거리보다 짧다면
             {
                 // 타겟 변경
-                minDistance = distance;
-                _target = enemy;
+                enemyMinDistance = distance;
+                _targetEnemy = enemy;
             }
         }
+
+        return enemyMinDistance;
     }
 }
