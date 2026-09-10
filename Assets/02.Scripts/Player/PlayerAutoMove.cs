990b564ebc8df36ede9fb34e28b002529570c3f6
qlsk <jhanggu0625@gmail.com>
2026-09-10 10:44:13 +0900

    feat: 자동 이동 시 가장 가까운 적에게 이동

--- Assets/02.Scripts/Player/PlayerAutoMove.cs
+++ Assets/02.Scripts/Player/PlayerAutoMove.cs
@@ -13,8 +13,23 @@ public class PlayerAutoMove : MonoBehaviour
     private void Update()
     {
         // 1. 타겟을 구한다.
-        GameObject target = GameObject.FindWithTag("Enemy");
-        if (target == null) return;
+        GameObject[] targets = GameObject.FindGameObjectsWithTag("Enemy");
+        if (targets.Length == 0) return;
+
+        GameObject target = targets[0];
+        float minDistance = float.MaxValue;
+        // 1-1. 가장 가까운 타겟을 찾는다.
+        foreach (GameObject enemy in targets)
+        {
+            // 거리를 구해서
+            float distance = Vector2.Distance(transform.position, enemy.transform.position);
+            if (distance < minDistance) // 저장된 거리보다 짧다면
+            {
+                // 타겟 변경
+                minDistance = distance;
+                target = enemy;
+            }
+        }
 
         // 2. 방향을 구한다.
         Vector3 direction = target.transform.position - transform.position;
